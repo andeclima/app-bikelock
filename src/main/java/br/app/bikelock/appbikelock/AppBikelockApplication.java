aec7f9a7acdecd09980e3073be1370d7d38654e8
Anderson Lima <anderson.lima@grupocev.com>
2021-02-08 15:48:42 -0300

    Inicial

---
+++ src/main/java/br/app/bikelock/appbikelock/AppBikelockApplication.java
@@ -0,0 +1,13 @@
+package br.app.bikelock.appbikelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBikelockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBikelockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/app/bikelock/appbikelock/AppBikelockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.app.bikelock.appbikelock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBikelockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
